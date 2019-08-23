cc30ee2da0c447f7e4f6b67a8b3eb56a6a80289d
liujintao <liujintaonb@outlook.com>
2019-08-23 18:00:35 +0800

    添加eurake服务注册者

---
+++ eurekadiscoveryclient/src/main/java/com/springcloud/eurekadiscoveryclient/web/HelloController.java
@@ -0,0 +1,27 @@
+package com.springcloud.eurekadiscoveryclient.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public  String index(){
+        ServiceInstance instance = client.getInstances("hello-service").get(0);
+        client.description();
+        logger.info("/hello,host:"+instance.getHost()+",service_id"+instance.getServiceId());
+        return "Hello World";
+    }
+
+
+}
